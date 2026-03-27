2411f5f17a2eac370e0e308437a37ccc9a4ded0f
jnbliss08 <jnbliss08@gmail.com>
2026-03-27 17:08:43 +0000

    commit

--- program_3.py
+++ program_3.py
@@ -1,18 +1,34 @@
 # Program #3: Average Numbers
 # Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
 # (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+# Write a program that reads all of the numbers stored in the file and calculates their total.
 
-# The program should handle the following exceptions: 
+# The program should handle the following exceptions:
 
 # It should handle any IOError exceptions that are raised.
-# It should handle any ValueError exceptions that are raised when the items that are read from the file 
+# It should handle any ValueError exceptions that are raised when the items that are read from the file
 # are converted to a number.
 def sum_numbers_from_file():
     ######################
-    # Add your code here #
+    with open("numbers.txt", "r") as f:
+        total = sum(int(line) for line in f if line.strip())
+        print (f'the sum of the file is {total}')
+    
+    try:
+        with open("numbers.txt", "r") as f:
+            total = sum(int(line) for line in f if line.strip())
+            for line in f:
+                line = line.strip
+                if not line:
+                    continue
+                try:
+                    total = sum(int(line) for line in f if line.strip())
+                except ValueError:
+                    print (f'{line.strip} is not a valid number')
+    except IOError:
+        print ('file not found')
     ######################
-    print('In the sum_numbers_from_file function')
+
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
